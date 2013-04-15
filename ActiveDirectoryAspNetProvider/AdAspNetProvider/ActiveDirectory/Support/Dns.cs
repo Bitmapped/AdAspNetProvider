0d277ab9ba68f20003ec9210e06ec1279cf56b90
Bitmapped_cp <SND\Bitmapped_cp>
2013-04-15 16:54:26 +0000

    Improved retry capability.

--- ActiveDirectoryAspNetProvider/AdAspNetProvider/ActiveDirectory/Support/Dns.cs
+++ ActiveDirectoryAspNetProvider/AdAspNetProvider/ActiveDirectory/Support/Dns.cs
@@ -10,15 +10,32 @@ namespace AdAspNetProvider.ActiveDirectory.Support
 {
     static public class Dns
     {
+
+        // Define cache for storing variables.
+        static private Dictionary<string, IPAddress[]> ipCache = new Dictionary<string, IPAddress[]>();
+
         /// <summary>
         /// Get IP addresses for the specified DNS host or IP address.
         /// </summary>
         /// <param name="host">Host to lookup.</param>
         /// <returns>IP addresses for host.</returns>
-        static public IEnumerable<IPAddress> GetIpAddresses(string host)
+        static public IPAddress[] GetIpAddresses(string host)
         {
+            // See if IP addresses are cached.
+            if (Dns.ipCache.ContainsKey(host) && Dns.ipCache[host].Any())
+            {
+                return Dns.ipCache[host];
+            }
+
+            // Values are not cached.  Load them up.
+            var ipAddresses = System.Net.Dns.GetHostAddresses(host);
+
+            // Store values in cache.
+            Dns.ipCache.Remove(host);
+            Dns.ipCache.Add(host, ipAddresses);
+
             // Get host entry.
-            return System.Net.Dns.GetHostAddresses(host).AsEnumerable();
+            return ipAddresses;
         }
     }
 }
